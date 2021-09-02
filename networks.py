fd964abf0dbc022aa949738c551cfd05cde08951
hanjiale <hanjiale@mail.ustc.edu.cn>
2021-09-02 15:34:00 +0800

    dqn has three convolution layers

--- networks.py
+++ networks.py
@@ -6,15 +6,17 @@ class Q_Network(nn.Module):
 
     def __init__(self, num_frame, num_action):
         super(Q_Network, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=num_frame, out_channels=16, kernel_size=8, stride=4, padding=2)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2)
-        self.fc1 = nn.Linear(32*9*9, 256)
-        self.fc2 = nn.Linear(256, num_action)
+        self.conv1 = nn.Conv2d(in_channels=num_frame, out_channels=32, kernel_size=8, stride=4, padding=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
+        self.fc1 = nn.Linear(64*7*7, 512)
+        self.fc2 = nn.Linear(512, num_action)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
-        x = x.view(-1, 9*9*32)
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, 64*7*7)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
