2663192d0c2867f538de0d5a96fc7624f873693e
hanjiale <hanjiale@mail.ustc.edu.cn>
2021-09-02 16:45:07 +0800

    fix network, one head for online, the other for target

--- networks.py
+++ networks.py
@@ -9,16 +9,26 @@ class Q_Network(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=num_frame, out_channels=32, kernel_size=8, stride=4, padding=2)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
-        self.fc1 = nn.Linear(64*7*7, 512)
-        self.fc2 = nn.Linear(512, num_action)
+        self.fc1_online = nn.Linear(64*7*7, 512)
+        self.fc2_online = nn.Linear(512, num_action)
+        self.fc1_target = nn.Linear(64*7*7, 512)
+        self.fc2_target = nn.Linear(512, num_action)
 
-    def forward(self, x):
+    def extract_feature(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
         x = x.view(-1, 64*7*7)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        return x
+
+    def online_forward(self, x):
+        x = F.relu(self.fc1_online(x))
+        x = self.fc2_online(x)
+        return x
+
+    def target_forward(self, x):
+        x = F.relu(self.fc1_target(x))
+        x = self.fc2_target(x)
         return x
 
 if __name__ == '__main__':
